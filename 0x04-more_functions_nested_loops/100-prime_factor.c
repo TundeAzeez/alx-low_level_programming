29e386b1be629d3a115cd0bdb62998127dc86a1d
TundeAzeez <abdulazeezbabatunde.aa@gmail.com>
2022-12-16 16:29:21 -0800

    Really tired

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include "main.h"
+
 /**
- * main - prints the largest prime factor of 612852475143
- * 
- * Return: always 0
- **/
+*main - prints the largest prime factor of 612852475143
+*
+*
+*Return: always 0
+**/
 
 int main(void)
 {
